c8d26f2d273348cf112292e23c71c678bf10e17a
Marco <MarcoDavi67@hotmail.com>
2024-07-10 16:28:08 -0700

    Empresas , empresa por id

--- MapaAPI/ApiMapa/Controllers/empresasController.cs
+++ MapaAPI/ApiMapa/Controllers/empresasController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiMapa.Context;
+using ApiMapa.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,35 +9,51 @@ namespace ApiMapa.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PrebaController : ControllerBase
+    public class empresasController : ControllerBase
     {
-        // GET: api/<PrebaController>
+        public MyDbContext Context { get; }
+
+        public empresasController(MyDbContext context)
+        {
+            Context = context;
+        }
+        // GET: api/<empresasController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<List<Empresa>>> ListarEmpresas()
         {
-            return new string[] { "value1", "value2" };
+
+            return Ok(await Context.Empresas
+                .ToListAsync());
         }
 
-        // GET api/<PrebaController>/5
+        // GET api/<empresasController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult> ObtenerEmpresa(int id)
         {
-            return "value";
+            var empresa = await Context.Empresas
+
+           .FirstOrDefaultAsync(e => e.IdEmpresa == id);
+            if (empresa == null)
+            {
+                return NotFound(); // Retorna HTTP 404 si no se encuentra la empresa
+            }
+
+            return Ok(empresa); // Retorna la empresa encontrada
         }
 
-        // POST api/<PrebaController>
+        // POST api/<empresasController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<PrebaController>/5
+        // PUT api/<empresasController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<PrebaController>/5
+        // DELETE api/<empresasController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
