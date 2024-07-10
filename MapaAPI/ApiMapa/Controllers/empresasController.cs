307d6627a7bbbe30e7a788649863419b60af3915
Marco <MarcoDavi67@hotmail.com>
2024-07-10 16:44:31 -0700

    Modifica metodos EmpresasController

--- MapaAPI/ApiMapa/Controllers/empresasController.cs
+++ MapaAPI/ApiMapa/Controllers/empresasController.cs
@@ -9,54 +9,46 @@ namespace ApiMapa.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class empresasController : ControllerBase
+    public class EmpresasController : ControllerBase
     {
         public MyDbContext Context { get; }
 
-        public empresasController(MyDbContext context)
+        public EmpresasController(MyDbContext context)
         {
             Context = context;
         }
-        // GET: api/<empresasController>
+
+        // GET: api/empresas
         [HttpGet]
         public async Task<ActionResult<List<Empresa>>> ListarEmpresas()
         {
-
-            return Ok(await Context.Empresas
-                .ToListAsync());
+            return Ok(await Context.Empresas.ToListAsync());
         }
 
-        // GET api/<empresasController>/5
+        // GET: api/empresas/5
         [HttpGet("{id}")]
         public async Task<ActionResult> ObtenerEmpresa(int id)
         {
-            var empresa = await Context.Empresas
-
-           .FirstOrDefaultAsync(e => e.IdEmpresa == id);
+            var empresa = await Context.Empresas.FirstOrDefaultAsync(e => e.IdEmpresa == id);
             if (empresa == null)
             {
-                return NotFound(); // Retorna HTTP 404 si no se encuentra la empresa
+                return NotFound();
             }
 
-            return Ok(empresa); // Retorna la empresa encontrada
+            return Ok(empresa);
         }
 
-        // POST api/<empresasController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        // GET: api/empresas/giro/{giro}
+        [HttpGet("giro/{giro}")]
+        public async Task<ActionResult> ObtenerEmpresaGiro(string giro)
         {
-        }
+            var empresas = await Context.Empresas.Where(e => e.Giro == giro).ToListAsync();
+            if (!empresas.Any())
+            {
+                return NotFound();
+            }
 
-        // PUT api/<empresasController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<empresasController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return Ok(empresas);
         }
     }
 }
